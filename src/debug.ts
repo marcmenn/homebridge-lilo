2dc35827ac84af981b94662e14940bc5a0e4d975
Marcus Mennemeier <marcus@mennemeier.de>
2022-03-18 10:16:15 +0100

    Refactor setting of logFunction

--- src/debug.ts
+++ src/debug.ts
@@ -1,21 +1,25 @@
 import Debugger from 'debug'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type LogFunction = (...args: any[]) => any
+
 const root = Debugger('LILO')
 const logger = [root]
-let { log } = root
+let log: LogFunction | null = null
 
 export default (namespace?: string): Debugger.Debugger => {
   if (namespace) {
+    root('Creating logger for namespace %s', namespace)
     const result = root.extend(namespace)
     logger.push(result)
-    result.log = log
+    if (log) result.log = log
     return result
   }
   return root
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const setLog = (logFn: (...args: any[]) => any) => {
+export const setLog = (logFn: LogFunction) => {
+  root('Redirecting log output for %d existing loggers', logger.length)
   log = logFn
   logger.forEach((l) => {
     // eslint-disable-next-line no-param-reassign
